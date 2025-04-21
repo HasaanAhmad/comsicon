ef1c708bb998f2e1f82470f25d0c542faa80456a
Hasaan Ahmad <hasaanahmad10023@gmail.com>
2025-04-21 12:27:29 +0500

    next themes

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import Providers from "@/components/Providers";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -28,8 +29,9 @@ export default function RootLayout({
       suppressHydrationWarning
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-
+        <Providers>
         {children}
+        </Providers>
       </body>
     </html>
   );
