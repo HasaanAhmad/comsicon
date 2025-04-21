ad517923bc6759f4f781f48e4348198ec21f732d
Hasaan Ahmad <hasaanahmad10023@gmail.com>
2025-04-21 12:02:39 +0500

    auth page

--- app/(root)/(auth)/authentication/page.tsx
+++ app/(root)/(auth)/authentication/page.tsx
@@ -1,9 +1,38 @@
-import React from 'react'
+import { auth } from "@/utils/auth"
+import { signOut, signIn } from "@/utils/auth"
 
-const page = () => {
-  return (
-    <div>Authentication page</div>
+const page = async () => {
+  const session = await auth()
+
+  const handleSignOut = async () => {
+    'use server'
+    await signOut()
+  }
+
+  const handleSignIn = async () => {
+    'use server'
+    await signIn('google')
+  }
+
+  return (  
+    <div>
+      {session?.user ? (
+        <div>
+          <h1>Welcome {session.user.name}</h1>
+          <form action={handleSignOut}>
+            <button type="submit">Sign Out</button>
+          </form>
+        </div>
+      ) : (
+        <div>
+          <div>Not Logged In</div>
+          <form action={handleSignIn}>
+            <button type="submit">Sign In</button>
+          </form>
+        </div>
+      )}
+    </div>
   )
-}
+} 
 
 export default page
